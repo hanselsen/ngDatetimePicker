2fd29872e3d11558f013aa6aea3fefac1d45942d
Hans van den Elsen <hans.van.den.elsen@cgi.com>
2015-07-23 11:49:30 +0200

    Min-date added multiple inputs with same model Styling Several bug fixes

--- src/esds-datetimepicker.js
+++ src/esds-datetimepicker.js
@@ -1,15 +1,23 @@
-var scripts = document.getElementsByTagName("script")
-var currentScriptPath = scripts[scripts.length-1].src;
 (function (app) {
+	var scripts = document.getElementsByTagName("script");
+	var currentScriptPath = scripts[scripts.length-1].src;
+
 	app.directive('esdsDatepicker',
 		['$rootScope', '$http', '$compile', '$filter', '$timeout',
 			function ($rootScope, $http, $compile, $filter, $timeout) {
 
-				var wrapperLoaded = false;
+				var wrapperContent = '';
 
 				return {
-					link: function(scope, elem, attr) {
+					restrict: 'A',
+					require: '?ngModel',
+					link: function(scope, elem, attr, ngModel) {
 						elem = angular.element(elem);
+						if(ngModel) elem.data('ngModel', ngModel);
+
+						if(elem.hasClass('esds-datepicker-input-group')) return;
+						if(elem.hasClass('esds-init')) return;
+						elem.toggleClass('esds-init', true);
 
 						$rootScope.esds = {};
 
@@ -23,7 +31,7 @@ var currentScriptPath = scripts[scripts.length-1].src;
 							var active = angular.element(this).parent().find('input');
 							updateVal(active);
 							$timeout(function () {
-								eval('delete scope.' + active.attr('esds-model'));
+								eval('delete active.scope().' + active.attr('esds-model'));
 							}, 0);
 						});
 
@@ -41,31 +49,67 @@ var currentScriptPath = scripts[scripts.length-1].src;
 						$rootScope.esds['showTime'] = elem.attr('esds-time') !== 'false';
 
 						var updateVal = function (elem) {
-							var vals = [];
-							if($rootScope.esds.showDate) vals.push($filter('date')($rootScope.esds.dateModel, 'dd-MM-yyyy'));
-							if($rootScope.esds.showTime) vals.push($filter('date')($rootScope.esds.timeModel, 'HH:mm'));
-							elem.val(vals.join(' ').trim());
-							elem.parent().find('.esds-delete-button').toggleClass('hidden', elem.val() == '');
+							var _updateVal = function (elem) {
+								var vals = [];
+								if(elem.attr('esds-date') !== 'false') vals.push($filter('date')($rootScope.esds.dateModel, 'dd-MM-yyyy'));
+								if(elem.attr('esds-time') !== 'false') vals.push($filter('date')($rootScope.esds.timeModel, 'HH:mm'));
+								elem.val(vals.join(' ').trim());
+								elem.parent().find('.esds-delete-button').toggleClass('hidden', elem.val() == '');
+								if(elem.data('ngModel')) {
+									eval('elem.scope().' + elem.attr('ng-model') + '="' + elem.val() + '"');
+									elem.data('ngModel').$setValidity('required', elem.val() != '');
+								}
+								if(elem.attr('esds-change')) {
+									eval('elem.scope().' + elem.attr('esds-change'));
+								}
+							};
+							if($('[esds-model="'+elem.attr('esds-model')+'"]').length == 0) {
+								_updateVal($(elem));
+							} else {
+								$('[esds-model="'+elem.attr('esds-model')+'"]').each(function () {
+									_updateVal($(this));
+								});
+							}
 						};
 						updateVal(elem);
 
-						$rootScope.showEsdsDatepicker = false;
-						scope.cancel = function () {
-							$rootScope.showEsdsDatepicker=false;
+						$('.esds-datepicker-object').toggleClass('hidden', true);
+						$rootScope.esds.cancel = function () {
+							$('.esds-datepicker-object').toggleClass('hidden', true);
 						};
-						scope.save = function () {
+						$rootScope.esds.save = function () {
 							var newDatetime = combineDateAndTime($rootScope.esds.dateModel, $rootScope.esds.timeModel);
 							var active = angular.element('.esds-active').removeClass('esds-active');
-							eval('active.scope().' + active.attr('esds-model') + ' = newDatetime');
-							updateVal(active);
-							$rootScope.showEsdsDatepicker=false;
+							if(active.length == 1) {
+								eval('active.scope().' + active.attr('esds-model') + ' = newDatetime');
+								updateVal(active);
+								$('[esds-mindate="'+active.attr('esds-model')+'"]').each(function () {
+									eval('var date = angular.element(this).scope().' + $(this).attr('esds-model'));
+									if(date < newDatetime) {
+										eval('angular.element(this).scope().' + $(this).attr('esds-model') + " = newDatetime");
+										updateVal(angular.element(this));
+									}
+								});
+
+								$('.esds-datepicker-object').toggleClass('hidden', true);
+							}
 						};
 
 						var done = function () {
 							elem.unbind('mousedown').bind('mousedown', function () {
+
+								$('.esds-datepicker-object').toggleClass('hidden', false);
+
 								var active = angular.element(this);
-								active.addClass('esds-active').blur();
-								eval('var date = scope.' + active.attr('esds-model'));
+								active.toggleClass('esds-active', true).blur();
+
+								$rootScope.esds.minDate = null;
+								if(active.attr('esds-mindate') != undefined) {
+									eval('var minDate = active.scope().' + active.attr('esds-mindate'));
+									$rootScope.esds.minDate = minDate;
+								}
+
+								eval('var date = active.scope().' + active.attr('esds-model'));
 
 								$rootScope.esds['dateModel'] = new Date();
 								$rootScope.esds['timeModel'] = new Date();
@@ -81,32 +125,46 @@ var currentScriptPath = scripts[scripts.length-1].src;
 									if (val.length == 2) {
 										$rootScope.esds.dateModel = strToDate(val[0]);
 										$rootScope.esds.timeModel = strToTime(val[1]);
-									} else if (scope.esds.showDate) {
+									} else if (active.scope().esds.showDate) {
 										$rootScope.esds.dateModel = strToDate(val[0]);
-									} else if (scope.esds.showTime) {
+									} else if (active.scope().esds.showTime) {
 										$rootScope.esds.timeModel = strToTime(val[0]);
 									}
 								}
 								$timeout(function () {
-									$rootScope.showEsdsDatepicker = true;
+									$('.esds-datepicker-object').toggleClass('hidden', false);
 								}, 0);
 							});
 						};
 
-						if(!wrapperLoaded) {
-                            wrapperLoaded = true;
+						var wrapper = $('.esds-datepicker-object');
+						if(wrapper.length == 1)
+							wrapper.remove();
+
+						var wrapperDone = function () {
+							var wrapper = angular.element(wrapperContent);
+							$('body').append(wrapper);
+							$compile(wrapper.contents())($rootScope);
+							$timeout(function () {
+								$('.esds-datepicker-object .pull-left, .esds-datepicker-object .pull-right').on('click', function () {
+									$('.esds-datepicker-object .text-muted').parent().hide();
+									$('.esds-datepicker-object .active').removeClass('active');
+								});
+								$('.esds-datepicker-object .text-muted').parent().hide();
+							}, 0);
+
+							done();
+						};
+						if(wrapperContent == '') {
 							$http({
 								method: 'get',
 								url: currentScriptPath.replace('esds-datetimepicker.js', 'wrapper.html')
 							}).then(function (res) {
-								var el = angular.element(res.data);
-								angular.element('body').prepend(el);
-								$compile(el.parent().contents())(scope);
-								el.removeClass('hidden');
-								done();
-							})
+								wrapperContent = res.data;
+								wrapperDone();
+							});
 						} else {
-							done();
+							wrapperDone();
 						}
 					}
 				};
